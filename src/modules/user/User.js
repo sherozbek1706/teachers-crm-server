76e3148d8a2224c75468e2c66aff22c3c99f4e38
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-29 14:20:39 +0500

    User uchun mongodb Model yasab oldim.

--- src/modules/user/User.js
+++ src/modules/user/User.js
@@ -5,3 +5,25 @@ const reqType = {
   required: true,
 };
 
+const UserSchema = new mongoose.Schema(
+  {
+    first_name: reqType,
+    last_name: reqType,
+    role: {
+      type: mongoose.SchemaTypes.String,
+      enum: ["employee", "admin"],
+      default: "employee",
+    },
+    age: {
+      type: mongoose.SchemaTypes.Number,
+      required: true,
+    },
+    username: {
+      ...reqType,
+      unique: true,
+    },
+    password: reqType,
+  }
+);
+const User = mongoose.model("user", UserSchema);
+module.exports = User;
