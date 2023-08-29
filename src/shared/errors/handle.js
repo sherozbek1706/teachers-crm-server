52ba8a6b97f501958bac63416310e640d19da0a3
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-29 13:18:09 +0500

    status berib chiqdim

--- src/shared/errors/handle.js
+++ src/shared/errors/handle.js
@@ -15,5 +15,11 @@ const {
 
 module.exports = (err, req, res, next) => {
   let status = 500;
+
+  if (err instanceof BadRequestError) status = 400;
+  else if (err instanceof UnauthorizedError) status = 401;
+  else if (err instanceof ForbiddenError) status = 403;
+  else if (err instanceof NotFoundError) status = 404;
+
   res.status(status).json({ error: err.message });
 };
