6f2288b9f79fd718215128bb47aba4756debaf0e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-29 13:15:19 +0500

    Datavazaga ulansa ulandi deb chiqadi

--- src/db/index.js
+++ src/db/index.js
@@ -4,6 +4,9 @@ const config = require("../shared/config");
 const db = () => {
   mongoose
     .connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`)
+    .then(() => {
+      console.log("SERVER HAS BEEN CONNECTED TO DATABASE SUCCESSFULL");
+    })
 };
 
 module.exports = db;
