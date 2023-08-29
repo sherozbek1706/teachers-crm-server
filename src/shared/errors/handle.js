c84574823bdebd5bace4612a39907bc426e661fe
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-29 13:17:58 +0500

    handlarni yasab oldim.

--- src/shared/errors/handle.js
+++ src/shared/errors/handle.js
@@ -6,3 +6,14 @@ const {
   ForbiddenError,
 } = require(".");
 
+/**
+ * @param {Error} err
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+
+module.exports = (err, req, res, next) => {
+  let status = 500;
+  res.status(status).json({ error: err.message });
+};
