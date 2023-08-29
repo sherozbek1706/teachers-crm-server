06ff89993be567c9972bcb0523f87046df7a5256
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-29 14:00:43 +0500

    cors initial qilindi!

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const config = require("./shared/config");
+const cors = require("cors");
 const app = express();
 
 // PORT aniqlab olish
 const PORT = config.port || 8080;
 
+// middleware
+app.use(cors());
 app.listen(PORT, () => {
   console.log(`SERVER HAS BEEN STARTED ON PORT:${PORT}`);
 });
