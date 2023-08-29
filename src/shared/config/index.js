1493df2278541b92837d5c6433bd6c62d5ca526c
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-29 12:57:52 +0500

    database malumotlarini berib yubordim

--- src/shared/config/index.js
+++ src/shared/config/index.js
@@ -2,6 +2,11 @@ require("dotenv/config");
 
 const config = {
   port: process.env.PORT,
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    name: process.env.DB_NAME,
+  },
 };
 
 module.exports = config;
