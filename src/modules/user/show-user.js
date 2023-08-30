553e4c2f9825b934d5d4e3536c1e865e08ef03ac
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-30 09:51:20 +0500

    console.log(user); keraksiz

--- src/modules/user/show-user.js
+++ src/modules/user/show-user.js
@@ -1,4 +1,5 @@
 const showUserServices = async ({ params, user }) => {
+  console.log(user);
   if (user) {
     if (user.role !== "admin") {
       throw new ForbiddenError("This user is not allowed this right!");
