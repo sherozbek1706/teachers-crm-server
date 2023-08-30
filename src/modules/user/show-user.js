b20badc7770782d1b6a4a206c5edaf988be8f8d3
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-30 09:51:08 +0500

    userni kelganmi kelmagan mi tekshirmoqchi mna. Agar kelgan bo'lsa bu role admin bo'lishi kerak

--- src/modules/user/show-user.js
+++ src/modules/user/show-user.js
@@ -1,4 +1,9 @@
 const showUserServices = async ({ params, user }) => {
+  if (user) {
+    if (user.role !== "admin") {
+      throw new ForbiddenError("This user is not allowed this right!");
+    }
+  }
 };
 
 module.exports = showUserServices;
