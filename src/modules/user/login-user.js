c31b457b6224aebe486d7bc8fe07622834c7de5c
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-30 09:18:09 +0500

    username databazadan topilmasa xato beradi.

--- src/modules/user/login-user.js
+++ src/modules/user/login-user.js
@@ -1,5 +1,12 @@
 const loginUserServices = async ({ body }) => {
   const { username, password } = body;
+
+  const existing = await User.findOne({ username });
+
+  if (!existing) {
+    throw new NotFoundError("User Not Found");
+  }
+
 };
 
 module.exports = loginUserServices;
