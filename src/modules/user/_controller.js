252fabd729d6da31cbf59fae883956b63888a74b
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-30 09:05:54 +0500

    addUser controllerini yasab oldim va errorlarni next funksiyaga berib yubordim.

--- src/modules/user/_controller.js
+++ src/modules/user/_controller.js
@@ -1,1 +1,15 @@
 const express = require("express");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+
+const addUser = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
