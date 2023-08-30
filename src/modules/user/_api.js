765266b3c7d998b1e776dde71fce4cffb929c079
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-30 09:52:56 +0500

    show userni routega bog'ladim.

--- src/modules/user/_api.js
+++ src/modules/user/_api.js
@@ -4,8 +4,11 @@ const { loginUser, addUser, listUser, showUser } = require("./_controller");
 
 const mAddUser = [isloggedIn, hasRole(["admin"])];
 const mListUser = [isloggedIn, hasRole(["admin"])];
+const mShowUser = [isloggedIn];
+
 router.post("/users/login", loginUser);
 router.post("/users", mAddUser, addUser);
 router.get("/users", mListUser, listUser);
+router.get("/users/:id", mShowUser, showUser);
 
 module.exports = router;
