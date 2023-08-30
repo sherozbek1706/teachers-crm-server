73ecd2eadd629d747690387846872d235925c7e0
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-30 08:49:25 +0500

    databazaga ulanib olish.

--- src/seed/index.js
+++ src/seed/index.js
@@ -12,6 +12,19 @@ const UsersSeed = require("./users-seed.js");
 
 const seedData = async () => {
   const uri = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to db");
+    })
+    .catch((err) => {
+      console.log("error", err);
+    });
+
 };
 
 seedData();
