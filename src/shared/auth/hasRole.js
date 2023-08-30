2a804f3a6fb96f4588e78e2da07930adaaab4aff
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-30 08:55:29 +0500

    has role funksiyasini ochib oldim.

---
+++ src/shared/auth/hasRole.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { ForbiddenError } = require("../errors");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+
+const hasRole = (roles) => {
+  return async (req, res, next) => {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+module.exports = hasRole;
