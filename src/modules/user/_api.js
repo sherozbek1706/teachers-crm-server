50c695b100a65c64bc7d54f754320393b21fe70f
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-30 09:23:28 +0500

    Login uchunrouterda yo'l yasab olaman va middlewaresiz ishlatib yuboraman.

--- src/modules/user/_api.js
+++ src/modules/user/_api.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
+
 const mAddUser = [isloggedIn, hasRole(["admin"])];
+router.post("/users/login", loginUser);
 router.post("/users", mAddUser, addUser);
 
 module.exports = router;
