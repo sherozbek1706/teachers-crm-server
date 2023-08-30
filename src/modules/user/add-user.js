c2b5dc38f4402f4c6d2a061a21ae6f5c593e69e5
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-30 09:08:22 +0500

    usernameni tekshirib oldim. Agar username oldin ishlatilgan bo'lsa xato beradi.

--- src/modules/user/add-user.js
+++ src/modules/user/add-user.js
@@ -1,5 +1,14 @@
 const User = require("./User");
+const { BadRequestError } = require("../../shared/errors");
 const addUserServices = async ({ body }) => {
+  const { username, password, ...data } = body;
+
+  const existed = await User.findOne({ username });
+
+  if (existed) {
+    throw new BadRequestError("This username already existed!");
+  }
+
 };
 
 module.exports = addUserServices;
