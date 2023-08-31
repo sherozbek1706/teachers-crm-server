3a71c61fa69cde4783a2a61f61596262a78f72ee
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-08-31 23:14:53 +0500

    Agar id orqali topilmasa Xato chiqaradi!

--- src/modules/user/edit-user.js
+++ src/modules/user/edit-user.js
@@ -1,4 +1,11 @@
 const User = require("./User");
+
+const editUserServices = async ({ body, params }) => {
+  const existing = await User.findOne({ _id: params.id });
+
+  if (!existing) {
+    throw new NotFoundError("User Not Found!");
+  }
 };
 
 module.exports = editUserServices;
