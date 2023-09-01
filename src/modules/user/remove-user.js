6b41cbf779bd4261a22badff59a194dc927c2e3d
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-01 11:50:31 +0500

    Agar id bo'yicha user topilmasa error beradi!

--- src/modules/user/remove-user.js
+++ src/modules/user/remove-user.js
@@ -3,5 +3,11 @@ const User = require("./User");
 const { NotFoundError, BadRequestError } = require("../../shared/errors");
 
 const removeUserServices = async ({ params, user }) => {
+  const existing = await User.findOne({ _id: params.id });
+
+  if (!existing) {
+    throw new NotFoundError("User Not Found!");
+  }
+
 };
 module.exports = removeUserServices;
