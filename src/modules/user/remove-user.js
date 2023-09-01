a9c3b60e1b21d1eba0f710c48a24b712217d59ed
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-01 11:50:11 +0500

    removeUserServices ochdim!

---
+++ src/modules/user/remove-user.js
@@ -0,0 +1,7 @@
+const User = require("./User");
+
+const { NotFoundError, BadRequestError } = require("../../shared/errors");
+
+const removeUserServices = async ({ params, user }) => {
+};
+module.exports = removeUserServices;
