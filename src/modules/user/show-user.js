6fb4255dc8688ceeb0bcff20c7fed531dd400142
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-01 11:24:51 +0500

    console.log olib tashlandi!

--- src/modules/user/show-user.js
+++ src/modules/user/show-user.js
@@ -3,7 +3,6 @@ const User = require("./User");
 const { NotFoundError, ForbiddenError } = require("../../shared/errors");
 
 const showUserServices = async ({ params, user }) => {
-  console.log(user);
   if (user) {
     if (user.role !== "admin") {
       throw new ForbiddenError("This user is not allowed this right!");
