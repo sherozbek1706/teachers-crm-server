4737ef7a7e764395dcf30e200b8491bb38b2b556
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-05 19:35:03 +0500

    showGuideService bo'ldim.

--- src/modules/guide/show-guide.js
+++ src/modules/guide/show-guide.js
@@ -1,2 +1,9 @@
+const Guide = require("./Guide");
 
+const showGuideService = async ({ params }) => {
+  const guide = await Guide.findById({ _id: params.id });
 
+  return { ...guide._doc, revision: 0 };
+};
+
+module.exports = showGuideService;
