89624d73691dc6b56586901bd082370d5e543e59
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-05 19:31:28 +0500

    Guidelarni show qilish uchun controller!

--- src/modules/guide/_controller.js
+++ src/modules/guide/_controller.js
@@ -30,3 +30,18 @@ const listGuide = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const showGuide = async (req, res, next) => {
+  try {
+    const result = await showGuideService({ params: req.params });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
