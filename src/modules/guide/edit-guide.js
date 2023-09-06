f712646db28d62ff82b8386923179be7bb274218
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-06 18:29:48 +0500

    Agar berilgan id bo'yicha Guide topilmasa NotFound beriladi!

--- src/modules/guide/edit-guide.js
+++ src/modules/guide/edit-guide.js
@@ -1,6 +1,12 @@
 const Guide = require("./Guide");
 
 const editGuideService = async ({ body, params }) => {
+  const existing = await Guide.findOne({ _id: params.id });
+
+  if (!existing) {
+    throw new NotFoundError("Guide Not Found!");
+  }
+
 };
 
 module.exports = editGuideService;
