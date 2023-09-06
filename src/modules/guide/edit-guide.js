f8737d6b28374f6d04f960cb406f1290e95d7d9e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-06 18:30:43 +0500

    Guideni edit qilib yuborish va return qilib berib yubordim.

--- src/modules/guide/edit-guide.js
+++ src/modules/guide/edit-guide.js
@@ -1,3 +1,4 @@
+const { NotFoundError } = require("../../shared/errors");
 const Guide = require("./Guide");
 
 const editGuideService = async ({ body, params }) => {
@@ -7,6 +8,13 @@ const editGuideService = async ({ body, params }) => {
     throw new NotFoundError("Guide Not Found!");
   }
 
+  const editGuide = await Guide.findByIdAndUpdate(
+    { _id: params.id },
+    { ...existing._doc, ...body },
+    { new: true }
+  );
+
+  return editGuide;
 };
 
 module.exports = editGuideService;
