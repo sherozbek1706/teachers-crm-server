6dcb880f97f9e53888ce9c741c1af4c66010874c
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-07 14:41:13 +0500

    version va id'larni configlari

--- src/modules/user-guide/UserGuide.js
+++ src/modules/user-guide/UserGuide.js
@@ -15,4 +15,14 @@ const UserGuideSchema = new mongoose.Schema(
       default: false,
     },
   },
+  {
+    versionKey: false,
+    id: true,
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
 );
