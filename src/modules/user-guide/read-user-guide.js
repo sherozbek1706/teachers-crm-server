f51b3629e29b9294d72e661177e33f4ac4105362
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-07 15:31:43 +0500

    Agar berilgan id'da mos guide topilmasa xato beradi!

--- src/modules/user-guide/read-user-guide.js
+++ src/modules/user-guide/read-user-guide.js
@@ -1,5 +1,13 @@
 const UserGuide = require("./UserGuide");
 
 const readUserGuideService = async ({ user, params }) => {
+  const list = UserGuide.find({ user_id: user.id });
+
+  const existing = await list.findOne({ _id: params.id, completed: false });
+
+  if (!existing) {
+    throw new NotFoundError("User Guide Not Found!");
+  }
+
 };
 module.exports = readUserGuideService;
