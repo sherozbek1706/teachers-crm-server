7ee009e4ffa02c51e90153f2dd8f4252583c346a
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-07 15:41:42 +0500

    guide qo'shayotganimizda notify true kelsa user guide yasab beradi . Hamma user uchun

--- src/modules/guide/add-guide.js
+++ src/modules/guide/add-guide.js
@@ -1,9 +1,27 @@
 const Guide = require("./Guide");
+const UserGuide = require("../user-guide/UserGuide");
+const User = require("../user/User");
 
 const addGuideServices = async ({ body }) => {
   const { notify = false, ...data } = body;
 
   const guide = await Guide.create({ ...data });
+
+  if (notify) {
+    const usersList = await User.find();
+    const usersGuideList = [];
+
+    usersList.forEach((user) => {
+      const userGuideObj = {
+        guide_id: guide.id,
+        user_id: user.id,
+      };
+
+      usersGuideList.push(userGuideObj);
+    });
+
+    await UserGuide.insertMany(usersGuideList);
+  }
 
   return { ...guide._doc, notified: notify };
 };
