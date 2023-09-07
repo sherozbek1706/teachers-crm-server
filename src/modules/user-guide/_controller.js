b12ca5539884cf9e446637e61a4dac8f01d9a541
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-07 15:22:25 +0500

    listUserGuides controller ochib oldim.

---
+++ src/modules/user-guide/_controller.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const listUserGuides = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
