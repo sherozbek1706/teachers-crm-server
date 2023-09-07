3ce12ce296d8f92a226670196f43f4a206d51a6e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-07 15:27:21 +0500

    listlarni UserGuide'dan qidirib return qilib yubordim.

--- src/modules/user-guide/list-user-guides.js
+++ src/modules/user-guide/list-user-guides.js
@@ -1,5 +1,14 @@
 const UserGuide = require("./UserGuide");
 
 const listUserGuideService = async ({ user }) => {
+  const list = await UserGuide.find({ user_id: user.id })
+    .populate({
+      path: "guide",
+      select: "-_id",
+    })
+    .select("-user_id")
+    .lean({ virtuals: true });
+
+  return list;
 };
 module.exports = listUserGuideService;
