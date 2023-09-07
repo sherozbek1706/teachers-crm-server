a6a83a59505ca38f5d90d8dcc7f95719c3a88253
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-09-07 15:40:08 +0500

    api routerlarni yasab oldim.

--- src/modules/user-guide/_api.js
+++ src/modules/user-guide/_api.js
@@ -1,5 +1,12 @@
+const { listUserGuides, readUserGuides } = require("./_controller");
+const { isloggedIn, hasRole } = require("../../shared/auth");
 const isMongoId = require("../../shared/validator/isMongoId");
 const router = require("express").Router();
 
+const mListUserGuides = [isloggedIn];
+const mReadUserGuides = [isloggedIn, isMongoId];
+
+router.get("/user-guides", mListUserGuides, listUserGuides);
+router.post("/user-guides/:id/read", mReadUserGuides, readUserGuides);
 
 module.exports = router;
